b6b8b5931d0729deec7f6a4fa78d7a68bfbb3d9c
Prasanna Venkat <to.srini@gmail.com>
2017-03-23 16:25:08 -0700

    Fix formatting of new event and display the center name with details

--- src/main/webapp/event-create/component.js
+++ src/main/webapp/event-create/component.js
@@ -13,10 +13,15 @@ angular.
         $http.get('/api/v1/centers').then(function (response) {
           console.log("centers:", response);
           self.centers = response.data;
+
+          // Construct a displayable key formed with city, state and country so that the center is unambiguous.
+          for (var id in self.centers) {
+            var center = self.centers[id];
+            center.key = center.cityName + ', ' + center.stateCode + ', ' + center.country;
+          }
         });
 
         self.createEvent = function (event) {
-
           var offeringsSelected = [];
           for (var property in event.offerings) {
             if (event.offerings.hasOwnProperty(property)) {
