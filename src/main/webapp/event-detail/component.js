9486dbd7702be9429f2c122b17003a00a36b8a1e
Prasanna Venkat <to.srini@gmail.com>
2017-03-22 17:39:02 -0700

    Fix event detail page

--- src/main/webapp/event-detail/component.js
+++ src/main/webapp/event-detail/component.js
@@ -9,8 +9,6 @@ angular.
        * Exports the following model information for the given eventId.
        *
        * event - the event object corresponding to the given eventId.
-       * requests - all the RideRequest objects corresponding to this event.
-       * matchedOffers - all the OfferRequestMatch objects corresponding to this event, grouped by RideOffer.
        */
       function EventDetailController($routeParams, $http) {
         var self = this;
@@ -18,9 +16,6 @@ angular.
         $http.get('/api/v1/events/' + $routeParams.eventId).then(function(response) {
           console.log("response of event lookup by id:", $routeParams.eventId, response);
           self.event = response.data;
-
-          self.event.departureDateAndTime = new Date(self.event.departureDateAndTime);
-          self.event.createdAt = new Date(self.event.createdAt);
         });
       }
     ]
